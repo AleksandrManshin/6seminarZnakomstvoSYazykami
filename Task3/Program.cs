22e7e153701523b62353172e6a8e154b7be4021e
Aleksandr <maivan007@yandex.ru>
2022-07-17 19:33:35 +0400

    Add Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿void Zadacha42()
+{
+    Console.WriteLine("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(Raschet(number));
+}
+
+int Raschet(int numb = 0)
+{
+    int result = 0;
+    int bias = 1;
+    while( numb > 0)
+    {
+        result += numb %2 * bias;
+        bias *=10;
+        numb /=2;
+    }
+    return result;
+}
+
+Zadacha42();
